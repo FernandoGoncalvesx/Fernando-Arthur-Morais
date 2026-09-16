9bde31b3e0ba98785334fac6c4cafa87ae41cc04
FernandoGoncalvesx <gcfernando007@gmail.com>
2026-09-16 10:27:32 -0300

    Atalizaççao

--- calculadora.py
+++ calculadora.py
@@ -4,16 +4,16 @@ class Processo:
         self.duracao = d
         self.restante = d
         
-        self.tempo_resposta = -1 
-        self.tempo_espera = 0
-        self.tempo_turnaround = 0
+        self.tempoResposta = -1 
+        self.tempoEspera = 0
+        self.tempoTurnaround = 0
         self.concluido = False
 
 def resultadoFinal(processos):
     n = len(processos) 
     
-    tr = sum(p.tempo_resposta for p in processos) / n
-    te = sum(p.tempo_espera for p in processos) / n
-    tt = sum(p.tempo_turnaround for p in processos) / n
+    tr = sum(p.tempoResposta for p in processos) / n
+    te = sum(p.tempoEspera for p in processos) / n
+    tt = sum(p.tempoTurnaround for p in processos) / n
     
     return f"{tr:.3f} {te:.3f} {tt:.3f}".replace(".", ",")
